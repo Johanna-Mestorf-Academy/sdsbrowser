8dbd13ce421c79671518c06c2ab1d3707f4d9023
Clemens Schmid <clemens@nevrome.de>
2018-10-05 12:32:01 +0200

    added custom css and github ribbon

--- R/app_sdsbrowser.R
+++ R/app_sdsbrowser.R
@@ -24,6 +24,21 @@ sdsbrowser <- function(
     # body
     body <- shinydashboard::dashboardBody(
       
+      # header
+      shiny::tags$head(
+        # include css
+        shiny::includeCSS(system.file("style/sdsbrowser_stylesheet.css", package = "sdsbrowser"))
+      ),
+      
+      shiny::HTML('
+        <a href = "https://github.com/nevrome/sdsbrowser">
+          <img style="position: absolute; top: 0; right: 0; border: 0; z-index:1000" 
+               src="https://camo.githubusercontent.com/e7bbb0521b397edbd5fe43e7f760759336b5e05f/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677265656e5f3030373230302e706e67" 
+               alt="Fork me on GitHub" 
+               data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png">
+        </a>  
+      '),
+      
       # intro
       shinydashboard::tabItems(
         shinydashboard::tabItem(
@@ -35,7 +50,9 @@ sdsbrowser <- function(
     
     # put sidebar and body together
     shinydashboard::dashboardPage(
-      shinydashboard::dashboardHeader(title = "sdsbrowser"),
+      shinydashboard::dashboardHeader(
+        title = "sdsbrowser"
+      ),
       sidebar,
       body
     )
