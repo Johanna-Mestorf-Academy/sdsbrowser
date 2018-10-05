6296055afdbee8dc92947688333f05f171491608
Clemens Schmid <clemens@nevrome.de>
2018-10-05 11:39:14 +0200

    first steps of ui layout

--- R/app_sdsbrowser.R
+++ R/app_sdsbrowser.R
@@ -10,6 +10,35 @@ sdsbrowser <- function(
   #### ui ####
   ui <- function() {
     
+    # sidebar menu
+    sidebar <- shinydashboard::dashboardSidebar(
+      shinydashboard::sidebarMenu(
+        id = "tabs",
+        shinydashboard::menuItem("Introduction", tabName = "intro", icon = shiny::icon("mortar-board"), selected = TRUE),
+        shinydashboard::menuItem("Table", tabName = "table", icon= shiny::icon("table")),
+        shinydashboard::menuItem("Plot", tabName = "plot", icon = shiny::icon("line-chart")),
+        shinydashboard::menuItem("About", tabName = "about", icon = shiny::icon("question"))
+      )
+    )
+    
+    # body
+    body <- shinydashboard::dashboardBody(
+      
+      # intro
+      shinydashboard::tabItems(
+        shinydashboard::tabItem(
+          tabName = "intro"
+        )
+      )
+      
+    )
+    
+    # put sidebar and body together
+    shinydashboard::dashboardPage(
+      shinydashboard::dashboardHeader(title = "sdsbrowser"),
+      sidebar,
+      body
+    )
   }
   
   #### server ####
