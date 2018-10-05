863864b01951d00f5c3ab855f6b72d9f6908b043
Clemens Schmid <clemens@nevrome.de>
2018-10-05 11:27:45 +0200

    removed example app and fixed check errors

--- R/app_sdsbrowser.R
+++ R/app_sdsbrowser.R
@@ -1,52 +1,19 @@
 #' sdsbrowser app
 #' 
+#' @param run_app Boolean. Should the app be run (TRUE - Default) or a shiny app object be returned?
+#' 
 #' @export
 sdsbrowser <- function(
-  startpanel = "Overview",
   run_app = TRUE
 ) {
   
   #### ui ####
   ui <- function() {
     
-    pageWithSidebar(
-      headerPanel('Iris k-means clustering'),
-      sidebarPanel(
-        selectInput('xcol', 'X Variable', names(iris)),
-        selectInput('ycol', 'Y Variable', names(iris),
-                    selected=names(iris)[[2]]),
-        numericInput('clusters', 'Cluster count', 3,
-                     min = 1, max = 9)
-      ),
-      mainPanel(
-        plotOutput('plot1')
-      )
-    )
-    
   }
   
   #### server ####
   server <- function(input, output, session) {
-    
-    # Combine the selected variables into a new data frame
-    selectedData <- reactive({
-      iris[, c(input$xcol, input$ycol)]
-    })
-    
-    clusters <- reactive({
-      kmeans(selectedData(), input$clusters)
-    })
-    
-    output$plot1 <- renderPlot({
-      palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
-                "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
-      
-      par(mar = c(5.1, 4.1, 0, 1))
-      plot(selectedData(),
-           col = clusters()$cluster,
-           pch = 20, cex = 3)
-      points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
-    })
     
   }
   
