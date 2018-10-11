53f1fb30156c566639d15d6c55ba726ecb2a97ec
Clemens Schmid <clemens@nevrome.de>
2018-10-11 17:02:48 +0200

    now with simplified lookup interface

--- R/server_table.R
+++ R/server_table.R
@@ -9,23 +9,7 @@ server_table <- function(input, output, session) {
       encoding = "Latin-1"
     ))
     
-    names(fb1) <- sdsanalysis::lookup_vars(names(fb1), 1)
-    
-    fb1_decoded <- purrr::map2_df(fb1, names(fb1), .f = sdsanalysis::lookup_attrs)
-    
-    # fb1_decoded <- dplyr::mutate_if(
-    #   fb1_decoded,
-    #   sapply(fb1_decoded, is.character),
-    #   as.factor
-    # )
-    
-    fb1_decoded <- dplyr::mutate_if(
-      fb1_decoded,
-      sapply(fb1_decoded, is.integer),
-      as.numeric
-    )
-    
-    fb1_decoded
+    sdsanalysis::lookup_everything(fb1, 1)
     
   })
   
