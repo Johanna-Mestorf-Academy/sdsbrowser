f37b6aa27788b6057043c20fd3c124c19e67add1
Clemens Schmid <clemens@nevrome.de>
2018-10-17 15:40:10 +0200

    more nice download buttons

--- R/ui_load_data.R
+++ R/ui_load_data.R
@@ -6,11 +6,23 @@ ui_load_data <- function(id) {
     2,
     shiny::h4("Available data"),
     shiny::uiOutput(ns("dataset_selection")),
-    shiny::actionButton(
-      "download_dataset", "Download data",
-      icon = shiny::icon("download")
+    fluidRow(
+      shiny::column(
+        6,
+        shiny::actionButton(
+          "download_dataset", "Download raw",
+          icon = shiny::icon("download")
+        )
+      ),
+      shiny::column(
+        6,
+        shiny::actionButton(
+          "download_dataset", "decoded",
+          icon = shiny::icon("download")
+        )
+      )
     ),
-    shiny::br(), shiny::br(), shiny::br()
+    shiny::br(), shiny::br()
   )
   
   
