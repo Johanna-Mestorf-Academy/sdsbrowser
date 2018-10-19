74a271584ad458ac796c8e0c8282ab8f34caa5ff
Clemens Schmid <clemens@nevrome.de>
2018-10-19 14:09:50 +0200

    started to work on IGerM plot

--- R/server_overview.R
+++ R/server_overview.R
@@ -3,7 +3,19 @@ server_overview <- function(input, output, session, current_dataset) {
   ns <- session$ns
   
   output$gf_plot <- plotly::renderPlotly({
-    plotly::plot_ly(data = datasets::iris, x = ~Sepal.Length, y = ~Petal.Length)
+    
+    sdsdata <- dplyr::mutate(
+      hu,
+      igerm_cat = sdsanalysis::lookup_IGerM_category(hu$index_geraete_modifikation, subcategory = TRUE)
+    )
+    
+    ggplot2::ggplot(sdsdata) +
+      ggplot2::geom_bar(
+        ggplot2::aes_string(x = "igerm_cat", fill = "index_geraete_modifikation")
+      ) +
+      ggplot2::scale_x_discrete(labels = function(x) stringr::str_wrap(x, width = 10)) +
+      ggplot2::coord_flip() +
+      theme_sds()
   })
   
   output$IGerM_plot <- plotly::renderPlotly({
