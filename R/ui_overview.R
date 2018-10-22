5a17679afcecfca22382543ed0e9b222fcc00859
Clemens Schmid <clemens@nevrome.de>
2018-10-22 15:37:40 +0200

    changed layout of overview page to accommodate an additional plot

--- R/ui_overview.R
+++ R/ui_overview.R
@@ -5,45 +5,57 @@ ui_overview <- function(id) {
   shiny::fluidPage(
     shiny::fluidRow(
       shiny::column(
-        width = 4,
-        shinydashboard::box(
-          width = NULL,
-          title = "Modified artefacts",
-          plotly::plotlyOutput(
-            ns("proportion_mod_plot")
+        width = 9,
+        shiny::fluidRow(
+          shiny::column(
+            width = 3,
+            shinydashboard::box(
+              width = NULL,
+              title = "Modified artefacts",
+              plotly::plotlyOutput(
+                ns("proportion_mod_plot")
+              )
+            )
+          ),
+          shiny::column(
+            width = 9,
+            shinydashboard::box(
+              width = NULL,
+              title = "IGerM - Indexgeraetemodifikation",
+              plotly::plotlyOutput(
+                ns("IGerM_plot")
+              )
+            )
+          )
+        ),
+        shiny::fluidRow(
+          shiny::column(
+            width = 7,
+            shinydashboard::box(
+              width = NULL,
+              title = "Amount of basic forms",
+              plotly::plotlyOutput(
+                ns("gf_plot")
+              )
+            )
+          ),
+          shiny::column(
+            width = 5,
+            shinydashboard::box(
+              width = NULL,
+              title = "Size classes",
+              plotly::plotlyOutput(
+                ns("size_classes_plot")
+              )
+            )
           )
         )
       ),
       shiny::column(
-        width = 8,
+        width = 3,
         shinydashboard::box(
           width = NULL,
-          title = "IGerM - Indexgeraetemodifikation",
-          plotly::plotlyOutput(
-            ns("IGerM_plot")
-          )
-        )
-      )
-    ),
-    shiny::fluidRow(
-      shiny::column(
-        width = 7,
-        shinydashboard::box(
-          width = NULL,
-          title = "Amount of basic forms",
-          plotly::plotlyOutput(
-            ns("gf_plot")
-          )
-        )
-      ),
-      shiny::column(
-        width = 5,
-        shinydashboard::box(
-          width = NULL,
-          title = "Size classes",
-          plotly::plotlyOutput(
-            ns("size_classes_plot")
-          )
+          title = "huhu"
         )
       )
     )
