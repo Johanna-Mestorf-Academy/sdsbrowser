86e384483bc26f31239c3999ddb6d639013027cf
Clemens Schmid <clemens@nevrome.de>
2018-11-27 16:23:46 +0100

    function to make data validation much more nice

--- R/helpers_data_interaction.R
+++ R/helpers_data_interaction.R
@@ -3,3 +3,10 @@ get_variable_complete_name <- function(short_name) {
   sdsanalysis::lookup_var_complete_names(short_name)
 }
 
+# data availability validiation
+check_for_relevant_columns <- function(x, data){
+  shiny::validate(shiny::need(
+    all(x %in% names(data)),
+    "Dataset does not contain all relevant variables to prepare this plot."
+  ))
+}
