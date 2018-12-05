2e764034614855455b6b571feaa4bdf23e9392f3
Clemens Schmid <clemens@nevrome.de>
2018-12-05 15:07:25 +0100

    better default plot label angle

--- R/helpers_plot.R
+++ R/helpers_plot.R
@@ -9,8 +9,8 @@ theme_sds <- function(background_colour = "white") {
       panel.grid.minor = ggplot2::element_line(colour = "darkgrey", size = 0.5),
       panel.grid.major = ggplot2::element_line(colour = "darkgrey", size = 1),
       # axis
-      axis.text.x = ggplot2::element_text(angle = 45, vjust = 1, hjust = 1), 
-      axis.text.y = ggplot2::element_text(angle = 45, vjust = -1, hjust = 1.5),
+      axis.text.x = ggplot2::element_text(angle = 20, vjust = 1, hjust = 1), 
+      axis.text.y = ggplot2::element_text(angle = 20, vjust = -1, hjust = 1.5),
       # legend
       legend.key = ggplot2::element_rect(fill = background_colour),
       legend.background = ggplot2::element_rect(fill = background_colour)
